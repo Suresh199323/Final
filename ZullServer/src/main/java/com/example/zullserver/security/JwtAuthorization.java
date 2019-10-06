69d8b582a2692cbb5a0e6b000b26d86d84951672
suresh chand <schand0276@gmail.com>
2019-10-07 00:18:43 +0530

    changed package of JwtAuthorization to security package

--- ZullServer/src/main/java/com/example/zullserver/security/JwtAuthorization.java
+++ ZullServer/src/main/java/com/example/zullserver/security/JwtAuthorization.java
@@ -1,19 +1,21 @@
 
-package com.example.zullserver;
+package com.example.zullserver.security;
+
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import static com.example.zullserver.Constant.SecurityConstant.HEADER_STRING;
-import static com.example.zullserver.Constant.SecurityConstant.SECRET;
-import static com.example.zullserver.Constant.SecurityConstant.TOKEN_PREFIX;
+
+import static com.example.zullserver.Constant.SecurityConstant.*;
+
 public class JwtAuthorization extends BasicAuthenticationFilter {
     public JwtAuthorization(AuthenticationManager authManager) {
         super(authManager);
